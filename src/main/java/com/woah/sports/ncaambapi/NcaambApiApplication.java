85532f21f89e4eeb27beeb054bea8ad5862a5f80
gavemaster <zacharyringhoff@gmail.com>
2024-02-01 08:16:48 -0500

    Initial commit

---
+++ src/main/java/com/woah/sports/ncaambapi/NcaambApiApplication.java
@@ -0,0 +1,13 @@
+package com.woah.sports.ncaambapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcaambApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcaambApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woah/sports/ncaambapi/NcaambApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woah.sports.ncaambapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcaambApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
